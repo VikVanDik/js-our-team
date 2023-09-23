90e18af572ceb53ee10571a37205ac17e9c1b064
VikVanDik <95vlc17@gmail.com>
2023-09-23 15:53:18 +0200

    [add] : Foreach loop

--- script/script.js
+++ script/script.js
@@ -39,3 +39,30 @@ const members = [
   }
   
 ]
+
+const container = document.getElementById('container')
+container.innerHTML = ``
+
+
+// 3. Creare un ciclo che stampi in console tutte le informazioni.
+// members.forEach(member => {
+//   console.log(member);
+// });
+
+// 4. Modificare il ciclo per far stampare le info in pagina, direttamente all'interno delle cards preparate in precedenza.
+
+members.forEach(member => {
+  container.innerHTML += `
+  <div class="card col-4 m-3" style="width: 18rem;">
+      <img src="./img/${member.foto}" class="card-img-top img" alt="">
+      <div class="info d-flex justify-content-center ">
+        <h5 class="d-flex name">${member.nome}</h5>
+        <h5 class="d-flex surname">${member.cognome}</h5>
+      </div>
+      <div class="d-flex justify-content-center">
+        <div class="role">${member.ruolo}</div>
+      </div>
+    </div>
+    `
+    console.log(member);
+});
